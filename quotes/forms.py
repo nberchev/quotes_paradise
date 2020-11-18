7ca684ef96a5ae09d13f12bba491eddd16deb214
Nikolay Berchev <ber4ev@abv.bg>
2020-11-18 19:07:22 +0200

    Add new look and feel of the app, removed some folders

--- quotes/forms.py
+++ quotes/forms.py
@@ -6,7 +6,7 @@ from quotes.models import Quote
 class QuoteForm(forms.ModelForm):
     author = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', }),)
     text = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control', }),)
-    image_url = forms.URLField(widget=forms.URLInput(attrs={'class': 'form-control', }),)
+    image_url = forms.URLField(required=False, widget=forms.URLInput(attrs={'class': 'form-control', }),)
 
     class Meta:
         model = Quote
