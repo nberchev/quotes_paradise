e0a1039c92b42a26dfffe1c0b609849a8f3a2633
Nikolay Berchev <ber4ev@abv.bg>
2020-11-19 22:02:16 +0200

    Added details and delete views/templates, created some permisssions

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from quotes.views import QuotesList, MyQuotes, CreateQuote
+from quotes.views import QuotesList, MyQuotes, CreateQuote, quote_details, DeleteQuote
 
 
 urlpatterns = [
     path('', QuotesList.as_view(), name='all quotes'),
     path('my_quotes/', MyQuotes.as_view(), name='my quotes'),
     path('create/', CreateQuote.as_view(), name='create quote'),
+    path('details/<int:pk>/', quote_details, name='quote details'),
+    path('delete/<int:pk>/', DeleteQuote.as_view(), name='delete quote'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -7,6 +7,15 @@ from quotes.forms import QuoteForm
 from quotes.models import Quote
 
 
+def has_user_access_to_modify(current_user, current_object):
+    profile_user = ProfileUser.objects.filter(user__pk=current_user.pk)[0]
+
+    if current_object.user == profile_user or current_user.is_superuser:
+        return True
+    else:
+        return False
+
+
 class QuotesList(generic.ListView):
     model = Quote
     template_name = 'quotes/quotes_list.html'
@@ -36,3 +45,32 @@ class CreateQuote(generic.CreateView):
         user_profile = ProfileUser.objects.filter(user__pk=self.request.user.id)[0]
         form.instance.user = user_profile
         return super().form_valid(form)
+
+
+class QuoteDetails(generic.DetailView):
+    model = Quote
+    template_name = 'quotes/quote_details.html'
+    context_object_name = 'quote'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteDetails, self).get_context_data(**kwargs)
+
+        if has_user_access_to_modify(self.request.user, self.get_object()):
+            context['is_user_author_or_admin'] = True
+        else:
+            context['is_user_author_or_admin'] = False
+
+        return context
+
+
+class DeleteQuote(generic.DeleteView):
+    model = Quote
+    template_name = 'quotes/delete_quote.html'
+    success_url = '/quotes/'
+
+    def get(self, request, pk):
+        if has_user_access_to_modify(request.user, self.get_object()):
+            quote = self.get_object()
+            return render(request, 'quotes/delete_quote.html', {'quote': quote})
+        else:
+            return render(request, 'shared/unauthorized.html')
